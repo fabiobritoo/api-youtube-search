78fe9f853d48e54c6675dbb4ec1a0ab754f132bd
Fábio Brito <fabio.brito@atlanteam.com.br>
2022-08-19 16:37:37 -0300

    Criada Função para Abrir URL

---
+++ youtube_link.py
@@ -0,0 +1,16 @@
+import requests
+from youtube_search import YoutubeSearch
+import json
+
+import webbrowser
+
+def findYT(search):
+
+    results = YoutubeSearch("trailer " + search, max_results=1).to_json()
+    url = json.loads(results)["videos"][0]["url_suffix"]
+    link = "https://www.youtube.com" + url
+
+    webbrowser.open_new(link)
+
+findYT("Home Alone")
+
